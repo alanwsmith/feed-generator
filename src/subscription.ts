5694d7b74829f90f2a3a3b71a5c72339426d2556
Alan Smith <commit@alanwsmith.com>
2023-05-18 23:12:24 -0400

    mysql db initial write in place

--- src/subscription.ts
+++ src/subscription.ts
@@ -3,50 +3,78 @@ import {
   isCommit,
 } from './lexicon/types/com/atproto/sync/subscribeRepos'
 import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
+import mysql from 'mysql2/promise';
+
 
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
+
   async handleEvent(evt: RepoEvent) {
+
+    const connection = await mysql.createConnection({
+      host: '127.0.0.1',
+      user: 'bluesky_feed',
+      password: process.env.MYSQL_PASS,
+      database: 'bluesky_alfa',
+    });
+
+
     if (!isCommit(evt)) return
     const ops = await getOpsByType(evt)
 
-    // This logs the text of every post off the firehose.
-    // Just for fun :)
-    // Delete before actually using
+
     for (const post of ops.posts.creates) {
-      console.log(post.record.createdAt)
+
+      // let hashtag = 0
+      // let cat = 0
+      // let dog = 0
+      // let butt = 0
+      // let space = 0
+      // let gifs = 0
+      // let music = 0
+      // let secret = 0
+      // let skeeet = 0
+      // let hellthread = 0
+      // let nsfw = 0
+      // let videos= 0
+
+
+
       console.log(post.record.text)
-      console.log(``)
+      connection.query('insert into posts (url, text, timestamp) values(?, ?, ?)',
+        [post.uri, post.record.text, post.record.createdAt.replace('Z', ' ').replace('Z', '')]);
     }
+    connection.end() 
 
-    const postsToDelete = ops.posts.deletes.map((del) => del.uri)
-    const postsToCreate = ops.posts.creates
-      .filter((create) => {
-        // only alf-related posts
-        return create.record.text.toLowerCase().includes('alf')
-      })
-      .map((create) => {
-        // map alf-related posts to a db row
-        return {
-          uri: create.uri,
-          cid: create.cid,
-          replyParent: create.record?.reply?.parent.uri ?? null,
-          replyRoot: create.record?.reply?.root.uri ?? null,
-          indexedAt: new Date().toISOString(),
-        }
-      })
+    // // const postsToDelete = ops.posts.deletes.map((del) => del.uri)
+    // // const postsToCreate = ops.posts.creates
+    //   // .filter((create) => {
+    //   //   // only alf-related posts
+    //   //   return create.record.text.toLowerCase().includes('alf')
+    //   // })
+    //   .map((create) => {
+    //     // map alf-related posts to a db row
+    //     return {
+    //       uri: create.uri,
+    //       cid: create.cid,
+    //       replyParent: create.record?.reply?.parent.uri ?? null,
+    //       replyRoot: create.record?.reply?.root.uri ?? null,
+    //       indexedAt: new Date().toISOString(),
+    //     }
+    //   })
 
-    if (postsToDelete.length > 0) {
-      await this.db
-        .deleteFrom('post')
-        .where('uri', 'in', postsToDelete)
-        .execute()
-    }
-    if (postsToCreate.length > 0) {
-      await this.db
-        .insertInto('post')
-        .values(postsToCreate)
-        .onConflict((oc) => oc.doNothing())
-        .execute()
-    }
+    // if (postsToDelete.length > 0) {
+    //   await this.db
+    //     .deleteFrom('post')
+    //     .where('uri', 'in', postsToDelete)
+    //     .execute()
+    // }
+    // if (postsToCreate.length > 0) {
+    //   await this.db
+    //     .insertInto('post')
+    //     .values(postsToCreate)
+    //     .onConflict((oc) => oc.doNothing())
+    //     .execute()
+    // }
+
   }
 }
